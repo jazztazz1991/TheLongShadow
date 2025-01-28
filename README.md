612db5815cf37d584c0f64332f036ac6ad53c3a4
Cody Chase <cchase1991@gmail.com>
2025-01-28 14:27:14 -0500

    Git Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TheLongShadow
+
+Developed with Unreal Engine 5
---
+++ Source/TheLongShadow/TheLongShadow.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheLongShadow.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheLongShadow, "TheLongShadow" );
+ 
