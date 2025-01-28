567abb9fe65fd7470871c28762f0dad033a0afb6
Cody Chase <cchase1991@gmail.com>
2025-01-28 15:52:15 -0500

    Project Setup

---
+++ Source/TheLongShadow/Public/BaseClass/TLSPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseClass/TLSCharacter.h"
+#include "TLSPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELONGSHADOW_API ATLSPlayerCharacter : public ATLSCharacter
+{
+	GENERATED_BODY()
+	
+};
